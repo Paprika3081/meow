ee5488141daa60f7e59bb960084e63528aee9e9d
Paprika3081 <tatiana.malinina6032001@gmail.com>
2024-02-06 23:54:15 +1100

    start

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,28 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const HomePage = () => {
   return (
-    <section
-      className={
-        "h-[calc(100vh-70px)] flex justify-center items-center flex-col"
-      }
-    >
+    <section className={'h-[calc(100vh-70px)] flex justify-center items-center'}>
       <Image
-        src={"/cow.png"}
+        src={'/cow.png'}
         width={300}
-        height={300}
-        alt={"cow"}
-        className={"md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px]"}
+        height={400}
+        alt={'cow'}
+        className={'md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px]'}
       />
-      <h4 className={""}>
-        Сделано с душой <br />
-        Качественно...
-      </h4>
-      <p>
-        Будь то, мясомолочная продукция, овощи, выпечка, полуфабрикаты. Совхоз
-        "Южно-Сахалинский" - уделяет особое внимание качеству в производстве
-        всеми любимой фирменной продукции.
-      </p>
+      <div className={'flex flex-col '}>
+        <h3 className={'text-left weight-normal text-5xl text-gray-400 font'}>Сделано с душой</h3>
+        <h1 className={'text-left weight-normal text-6xl text-amber-950'}> Качественно...</h1>
+        <p className={'text-left weight-normal text-lg text-amber-950 '}>
+          Будь то, мясомолочная продукция, овощи, выпечка, полуфабрикаты. Совхоз "Южно-Сахалинский"
+          - уделяет особое внимание качеству в производстве всеми любимой фирменной продукции.
+        </p>
+      </div>
     </section>
   );
 };
--- app/_components/navbar/navbar-items.jsx
+++ app/_components/navbar/navbar-items.jsx
@@ -1,20 +1,30 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const items = [
   {
     id: 1,
-    title: "Домашняя",
-    path: "/",
+    title: 'ГЛАВНАЯ',
+    path: '/',
   },
   {
     id: 2,
-    title: "О нас",
-    path: "/about",
+    title: 'ИСТОРИЯ',
+    path: '/history',
   },
   {
     id: 3,
-    title: "Контакты",
-    path: "/contacts",
+    title: 'КАТАЛОГ',
+    path: '/catalog',
+  },
+  {
+    id: 4,
+    title: 'КОНТАКТЫ',
+    path: '/contacts',
+  },
+  {
+    id: 3,
+    title: 'ДОПОЛНИТЕЛЬНО',
+    path: '/',
   },
 ];
 
@@ -22,15 +32,12 @@ const NavbarItems = ({ pathname }) => {
   return (
     <div
       className={
-        "flex flex-col absolute right-0 gap-1 list-none bg-white shadow-2xl p-3 md:flex-row md:gap-4 md:static md:bg-transparent md:shadow-none"
+        'flex flex-col cursor-pointer absolute right-0 gap-1 list-none bg-white shadow-2xl p-3 md:flex-row md:gap-4 md:static md:bg-transparent md:shadow-none'
       }
     >
       {items.map((item) => (
         <li key={item.id}>
-          <Link
-            className={`${pathname === item.path ? "text-amber-500" : ""}`}
-            href={item.path}
-          >
+          <Link className={`${pathname === item.path ? 'text-amber-500' : ''}`} href={item.path}>
             {item.title}
           </Link>
         </li>
