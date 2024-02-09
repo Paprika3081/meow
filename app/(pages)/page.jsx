8b0c23b18f524ad528ea2a3fc6ec7868490de72b
IT-2 <C:\Users\IT\AppData\Roaming\The Bat!>
2024-02-09 11:55:52 +1100

    glavnaya-update-all

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -42,16 +42,92 @@ const HomePage = () => {
               </p>
             </div> 
     </section>
-    <section>
-      <div className="grid grid-cols-5 grid-rows-5 gap-4">
-      <div className="row-span-2 col-start-2 row-start-2">2</div>
-      <div className="row-span-2 col-start-3 row-start-2">3</div>
-      <div className="row-span-2 col-start-4 row-start-2">4</div>
-      <div className="row-span-2 col-start-4 row-start-4">5</div>
-      <div className="row-span-2 col-start-3 row-start-4">6</div>
-      <div className="row-span-2 col-start-2 row-start-4">7</div>
-</div>
+    <section className="bg-stone-600">
+      <div className="grid grid-cols-5 grid-rows-5 gap-4 ">
+        <div className="row-span-2 col-start-2 row-start-2">
+          <Image 
+           src="/cow-grid.png"
+           width={200}
+           height={250}
+           alt={'cow1'}
+          />
+        </div>
+        <div className="row-span-2 col-start-3 row-start-2  bg-stone-50">
+          <Image
+            src="/iconka-list.png" 
+            width={50}
+            height={50}
+            alt="iconka" />
+          <h3>Органическое земледелие</h3>
+          <p>
+            В сердце нашего хозяйства бьется понимание важности
+            устойчивости и заботы о природе. Мы гордимся тем, что в наших
+            пастбищах животные пасутся на свежем воздухе, получают
+            высококачественный корм{" "}
+          </p>
+        </div>
+        <div className="row-span-2 col-start-4 row-start-2">
+          <Image
+            src="/potato-grid.png"
+            width={600}
+            height={700} 
+            className=" items-center object-cover"
+            alt="#"          
+          />
+        </div>
+        <div className="row-span-2 col-start-4 row-start-4  bg-stone-50">
+          <Image 
+           src="/iconka-tractor.png" 
+           width={50}
+           height={50}
+           alt="iconka" 
+           />
+          <h3>Земледелие</h3>
+          <p>
+            Наша команда состоит из преданных профессионалов, способных
+            поддерживать баланс между современными технологиями и
+            традиционными методами фермерства.
+          </p>
+        </div>
+        <div className="row-span-2 col-start-3 row-start-4">
+          <Image
+            src="/about.png"
+            width={200}
+            height={250} 
+            alt="#"          
+          />
+        </div>
+        <div className="row-span-2 col-start-2 row-start-4  bg-stone-50">
+          <Image 
+            src="/iconka-cow.png" 
+            width={50}
+            height={50}
+            alt="iconka" 
+            />
+          <h3>Животноводство</h3>
+          <p>
+              Создаем уникальную среду для заботы о наших четвероногих
+              друзьях и обеспечения высококачественных продуктов.
+          </p>
+        </div>
+     </div>
     </section>
+    <section className="flex justify-center items-center flex-col w-[550px]">
+        <p className="">
+           ❝ Совхоз Южно-Сахалинский – это не только место производства, но и
+            настоящая семья, где каждый член команды вносит свой вклад в общее
+            благополучие. Мы приглашаем вас окунуться в атмосферу гармонии с
+            природой, где труд и любовь создают уникальные продукты, способные
+            радовать вас своим вкусом и полезными свойствами ❞
+         </p>
+         <h3 className="">
+           Добро пожаловать – в место, где забота о природе становится вкусом
+           жизни!
+          </h3>
+
+        <button>каталог</button>
+    </section>
+
     </>
    
   );
