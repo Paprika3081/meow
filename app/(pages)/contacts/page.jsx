699e351c545527252d763f92d281445de02b4bdf
IT-2 <C:\Users\IT\AppData\Roaming\The Bat!>
2024-02-12 12:47:28 +1100

    contacts-page

--- app/(pages)/contacts/page.jsx
+++ app/(pages)/contacts/page.jsx
@@ -1,5 +1,63 @@
+
+
 const ContactsPage = () => {
-  return "Contacts";
+  return (
+    <>
+    <div>
+      <h3>
+        Добро пожаловать
+      </h3>
+      <h1>
+        Связаться <br /> с нами
+      </h1>
+    </div>
+    <div>
+      <ul>
+       <h4>Главный офис</h4>
+       <h5>Адрес:</h5>
+       <li>Сахалинская обл., г. Южно-Сахалинск, пл /р-н Луговое, ул. Дружбы, дом №75</li>
+       <h5>Приемная:</h5>
+       <li>Тел: +7 (4242) 79-01-54</li>
+       <li>Email: office@agrosakhalin.ru</li>
+      </ul>
+      <ul>
+       <h5>Режим работы:</h5>
+       <li>Пн - Пт с 9:00 до 18:00</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Контакты</h4>
+        <p>Генеральный директор - Богославец Наталья Леонтьевна</p>
+        <p>
+         Отдел реализации -<a href="paprikaa666@gmail.com"> paprikaa666@gmail.com</a>
+        </p>
+        <p>
+           Секретарь -<a href="paprikaa666@gmail.com"> office@agrosakhalin.ru</a>
+         </p>
+         <p>
+           Юридический отдел -<p>Стёпин Владимир Евгеньевич</p>
+           <a href="paprikaa666@gmail.com"> stepin.v@agrosakhalin.ru</a>
+          </p>
+          <p>
+            Отдел бухгалтерия -<a href="paprikaa666@gmail.com"> paprikaa666@gmail.com</a>
+          </p>
+          <p>
+           Отдел информационных технологий -
+           <a href="paprikaa666@gmail.com"> admin@agrosakhalin.ru</a>
+           </p>
+          <p>
+            Отдел кадров -<a href="paprikaa666@gmail.com"> ok@agrosakhalin.ru</a>
+          </p>
+          <div>
+          <p>
+            Хотите работать у нас?
+            Нажмите сюда hh.ru
+          </p>
+
+    </div>
+    </div>
+    </>
+  );
 };
 
 export default ContactsPage;
