56a0253b6ddfa6aeb3e5418d95b00db681115dc8
your_username <your_email@gmail.com>
2024-02-15 09:39:48 +1100

    refactoring/about-block

--- app/(pages)/(home)/_components/about-block/ui/about-block.jsx
+++ app/(pages)/(home)/_components/about-block/ui/about-block.jsx
@@ -3,18 +3,33 @@ import Image from "next/image"
 const AboutBlock = () => {
   return (
     <section
-      className={"flex items-center justify-center gap-x-8 bg-neutral-200 p-10"}
+      className={
+        "flex flex-col items-center justify-center gap-x-8 bg-neutral-200 p-10 md:flex-row"
+      }
     >
       <Image
         src={"/cowcow.jpg"}
         width={400}
         height={610}
         alt={"cowcow"}
-        className={"rounded-3xl"}
+        className={"h-1/2 w-full rounded-3xl md:w-1/2"}
       />
-      <div className={"flex w-[550px]  flex-col justify-center "}>
-        <h2 className={"text-[40px] text-white"}>С любовью</h2>
-        <h2 className={"text-[100px]  text-amber-950 "}>О нас</h2>
+      <div
+        className={
+          "flex h-1/2 w-full flex-col items-center justify-center gap-8 md:items-start md:justify-start md:gap-20"
+        }
+      >
+        <h2 className={"relative text-3xl text-white md:text-6xl"}>
+          С любовью
+          <span
+            className={
+              "absolute left-2 top-4 text-5xl  font-bold text-amber-950 md:top-6 md:text-8xl"
+            }
+          >
+            О нас
+          </span>
+        </h2>
+
         <p className={"text-justify"}>
           Совхоз Южно-Сахалинский – великолепное место, где сливаются
           трудолюбие, опыт и любовь к земле в единое творческое стремление. Мы
