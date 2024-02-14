0e39dffb21a9b73d3b8ab0beadad9211735622b4
IT-2 <C:\Users\IT\AppData\Roaming\The Bat!>
2024-02-15 09:59:49 +1100

    structure-history

---
+++ app/(pages)/history/_components/ui/history-title.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+const HistoryTitle = () => {
+  return (
+   <div className="">
+          <h3 className={"text-[40px]"}>Совхоз "Южно-Сахалинский"</h3>
+          <h1 className={"text-[100px]"}>История</h1>
+        </div>
+  )
+}
+
+export {HistoryTitle}
+
