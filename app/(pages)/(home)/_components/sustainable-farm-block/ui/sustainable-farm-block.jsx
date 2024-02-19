cebd81f6c9be8a13c747185f3ab2a51edfb595ae
IT-2 <C:\Users\IT\AppData\Roaming\The Bat!>
2024-02-20 10:38:53 +1100

    final-glavnaya-page-twcss

--- app/(pages)/(home)/_components/sustainable-farm-block/ui/sustainable-farm-block.jsx
+++ app/(pages)/(home)/_components/sustainable-farm-block/ui/sustainable-farm-block.jsx
@@ -12,45 +12,48 @@ const SustainableFarmBlock = () => {
             className={"absolute rounded-lg object-cover"}
           />
         </div>
+        <div className=" bg-white  relative rounded-lg flex items-center justify-center flex-col">
+         <img src="/iconka-list.png" alt="iconka" width={70} />
+          <h3 className="font-bold">Органическое земледелие</h3>
+          <p className="text-center">
+            В сердце нашего хозяйства бьется понимание важности
+                  устойчивости и заботы о природе. Мы гордимся тем, что в наших
+                  пастбищах животные пасутся на свежем воздухе, получают
+                  высококачественный корм
+          </p>
+        </div>
         <div className="relative rounded-lg">
           <Image
-            src={"/cow-grid.png"}
+           src="/potato-grid.png"
             alt="cow"
             fill
             className={"absolute rounded-lg object-cover"}
           />
         </div>
+        <div className="relative rounded-lg flex items-center flex-col justify-center bg-white">
+          <img src="/iconka-cow.png" alt="iconka" width={70} />
+          <h3 className="font-bold">Животноводство</h3>
+          <p className="text-center">
+            Создаем уникальную среду для заботы о наших четвероногих
+            друзьях и обеспечения высококачественных продуктов.
+          </p>
+        </div>
         <div className="relative rounded-lg">
           <Image
-            src={"/cow-grid.png"}
+            src="/about.png"
             alt="cow"
             fill
             className={"absolute rounded-lg object-cover"}
           />
         </div>
-        <div className="relative rounded-lg">
-          <Image
-            src={"/cow-grid.png"}
-            alt="cow"
-            fill
-            className={"absolute rounded-lg object-cover"}
-          />
-        </div>
-        <div className="relative rounded-lg">
-          <Image
-            src={"/cow-grid.png"}
-            alt="cow"
-            fill
-            className={"absolute rounded-lg object-cover"}
-          />
-        </div>
-        <div className="relative rounded-lg">
-          <Image
-            src={"/cow-grid.png"}
-            alt="cow"
-            fill
-            className={"absolute rounded-lg object-cover"}
-          />
+        <div className="relative rounded-lg bg-white flex items-center flex-col justify-center">
+          <img src="/iconka-tractor.png" alt="iconka" width={70} />
+          <h3 className="font-bold">Земледелие</h3>
+          <p className="text-center">
+            Наша команда состоит из преданных профессионалов, способных
+            поддерживать баланс между современными технологиями и
+            традиционными методами фермерства.
+          </p>
         </div>
       </div>
     </section>
--- app/(pages)/history/_components/ui/history-title.jsx
+++ app/(pages)/history/_components/ui/history-title.jsx
@@ -7,8 +7,8 @@ const Yeseva = Yeseva_One({weight: ["400"], subsets: ["cyrillic", "latin"]})
 const HistoryTitle = () => {
   return (
    <div className="">
-          <h3 className={` text-gray-400 text-[40px] ${Yeseva.className}`}>Совхоз "Южно-Сахалинский"</h3>
-          <h1 className={`text-[100px] flex items-center justify-center ${lobster.className}`}>История</h1>
+          <h3 className={` text-gray-200 text-[40px] ${Yeseva.className}`}>Совхоз "Южно-Сахалинский"</h3>
+          <h1 className={`text-[100px] text-amber-1000 flex items-center justify-center ${lobster.className}`}>История</h1>
         </div>
   )
 }
