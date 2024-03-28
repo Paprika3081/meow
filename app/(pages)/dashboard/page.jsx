3a65fa300f005b6519622716aadea61c0a71b1a5
your_username <your_email@gmail.com>
2024-03-28 10:34:01 +0300

    feat/product-form

---
+++ app/(pages)/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import { ProductForm } from "./_components/product-from"
+
+const Dashboard = () => {
+  return <div className="w-screen h-screen">
+    <ProductForm/>
+  </div>
+}
+
+export default Dashboard
