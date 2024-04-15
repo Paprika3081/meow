267dcecdf272955bdfdf069e3bc732fb62aa23bb
Paprika3081 <tatiana.malinina6032001@gmail.com>
2024-04-15 11:13:11 +1100

    public

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-export default nextConfig
+const nextConfig = {
+    distDir: 'out' // Путь для сохранения собранных файлов
+  };
+  
+  export default nextConfig;
+  
