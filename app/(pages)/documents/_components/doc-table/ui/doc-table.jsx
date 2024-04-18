64c6fc188db2ef5d121a1e12f561b04dc6f15bdf
Paprika3081 <tatiana.malinina6032001@gmail.com>
2024-04-18 22:02:52 +1100

    /

--- app/(pages)/documents/_components/doc-table/ui/doc-table.jsx
+++ app/(pages)/documents/_components/doc-table/ui/doc-table.jsx
@@ -25,6 +25,7 @@ const DocTable = () => {
 
     fetchData(); // Вызываем функцию получения данных
   }, []);
+
   if (typeof window !== 'undefined') {
     // Ваш код, использующий window
   }
@@ -35,63 +36,63 @@ const DocTable = () => {
         <h3 className={`text-lg md:text-xl lg:text-2xl font-semibold ${Yeseva.className} text-gray-400`}>Декларации соответствия</h3>
       </div>
       <div className="overflow-x-auto">
-    <table className="min-w-full text-sm sm:text-base md:text-base lg:text-lg xl:text-lg">
-        <thead>
+        <table className="min-w-full text-sm sm:text-base md:text-base lg:text-lg xl:text-lg">
+          <thead>
             <tr className="bg-[#f8f2e5]">
-                <th className="px-2 sm:px-4 py-1 sm:py-2">Продукция</th>
-                <th className="px-2 sm:px-4 py-1 sm:py-2">Регистрационный номер</th>
-                <th className="px-2 sm:px-4 py-1 sm:py-2">Дата регистрации</th>
-                <th className="px-2 sm:px-4 py-1 sm:py-2">Действительна по</th>
-                <th className="px-2 sm:px-4 py-1 sm:py-2">PDF</th>
+              <th className="px-2 sm:px-4 py-1 sm:py-2">Продукция</th>
+              <th className="px-2 sm:px-4 py-1 sm:py-2">Регистрационный номер</th>
+              <th className="px-2 sm:px-4 py-1 sm:py-2">Дата регистрации</th>
+              <th className="px-2 sm:px-4 py-1 sm:py-2">Действительна по</th>
+              <th className="px-2 sm:px-4 py-1 sm:py-2">PDF</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             {/* Маппим данные из состояния */}
             {data.map((item, index) => (
-                <tr key={item.id} className={index % 2 === 0 ? 'bg-[#fffcf6]' : 'bg-[#f8f2e5]'}>
-                    <td className="px-2 sm:px-4 py-1 sm:py-2">{item.products}</td>
-                    <td className="px-2 sm:px-4 py-1 sm:py-2">{item.number}</td>
-                    <td className="px-2 sm:px-4 py-1 sm:py-2">{item.date}</td>
-                    <td className="px-2 sm:px-4 py-1 sm:py-2">{item.valid}</td>
-                    <td className="px-2 sm:px-4 py-1 sm:py-2">
-                        {/* Вместо ссылки на PDF, можно использовать URL, предоставленный в данных */}
-                        <a href={item.link} download className={`text-black hover:text-red-700 `}>
-                            Скачать PDF
-                        </a>
-                    </td>
-                </tr>
+              <tr key={item.id} className={index % 2 === 0 ? 'bg-[#fffcf6]' : 'bg-[#f8f2e5]'}>
+                <td className="px-2 sm:px-4 py-1 sm:py-2">{item.products}</td>
+                <td className="px-2 sm:px-4 py-1 sm:py-2">{item.number}</td>
+                <td className="px-2 sm:px-4 py-1 sm:py-2">{item.date}</td>
+                <td className="px-2 sm:px-4 py-1 sm:py-2">{item.valid}</td>
+                <td className="px-2 sm:px-4 py-1 sm:py-2">
+                  {/* Вместо ссылки на PDF, можно использовать URL, предоставленный в данных */}
+                  <a href={item.link} download className={`text-black hover:text-red-700 `}>
+                    Скачать PDF
+                  </a>
+                </td>
+              </tr>
             ))}
-        </tbody>
-    </table>
-</div>
+          </tbody>
+        </table>
+      </div>
       {/* Остальная часть компонента остается без изменений */}
       <div className="mt-8">
-    <table className="min-w-full">
-        <thead>
+        <table className="min-w-full">
+          <thead>
             <tr className="bg-[#f8f2e5]">
-                <th className="px-4 sm:text-base md:text-base lg:text-lg xl:text-lg py-2">Полезные документы</th>
+              <th className="px-4 sm:text-base md:text-base lg:text-lg xl:text-lg py-2">Полезные документы</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             <tr className="bg-[#fffcf6]">
-                <td className="px-4 py-2">
-                    <a href="/Dogovor_prodazhi_produktsii.docx" download className={`text-black sm:text-base md:text-base lg:text-lg xl:text-lg hover:text-red-700 ${window.innerWidth < 640 ? 'underline' : ''}`}>
-                        Договор продажи продукции собственного производства
-                    </a>
-                </td>
+              <td className="px-4 py-2">
+                <a href="/Dogovor_prodazhi_produktsii.docx" download className={`text-black sm:text-base md:text-base lg:text-lg xl:text-lg hover:text-red-700 ${typeof window !== 'undefined' && window.innerWidth < 640 ? 'underline' : ''}`}>
+                  Договор продажи продукции собственного производства
+                </a>
+              </td>
             </tr>
             <tr className="bg-[#f8f2e5]">
-                <td className="px-4 py-2">
-                    <a href="/details.pdf" download className={`text-black sm:text-base md:text-base lg:text-lg xl:text-lg hover:text-red-700 `}>
-                        Реквизиты АО "Совхоз Южно-Сахалинский"
-                    </a>
-                </td>
+              <td className="px-4 py-2">
+                <a href="/details.pdf" download className={`text-black sm:text-base md:text-base lg:text-lg xl:text-lg hover:text-red-700 `}>
+                  Реквизиты АО "Совхоз Южно-Сахалинский"
+                </a>
+              </td>
             </tr>
-        </tbody>
-    </table>
-</div>
-</div>
- );
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DocTable;
