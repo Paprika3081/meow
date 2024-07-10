83f8c64fe66e9a2469a26bbd3d7140cf9fa62786
Paprika3081 <tatiana.malinina6032001@gmail.com>
2024-07-11 09:40:32 +1100

    image-fix

--- app/(pages)/(home)/_components/sustainable-farm-block/ui/sustainable-farm-block.jsx
+++ app/(pages)/(home)/_components/sustainable-farm-block/ui/sustainable-farm-block.jsx
@@ -18,7 +18,7 @@ const SustainableFarmBlock = () => {
         <div className="relative rounded-lg overflow-hidden">
           <div className="w-full h-full">
             <Image
-              src="/about123.png"
+              src="/flovers.png"
               alt="Изображение с коровой"
               quality={100}
               width={500}
